a8f610b0fb59cc80fd1a590f9bb9476314561d45
lihua3 <738183495@qq.com>
2019-10-08 14:55:44 +0800

    add easy 58 66

---
+++ src/easy/num_58.java
@@ -0,0 +1,35 @@
+package easy;
+
+public class num_58 {
+    public static int lengthOfLastWord(String s) {
+        s = s.trim();
+        String[] splitString = s.split(" ");
+        if (splitString.length > 0 && !"".equals(splitString[splitString.length-1])){
+            String res = splitString[splitString.length-1];
+            return res.length();
+        }else{
+            return 0;
+        }
+    }
+
+    public static int lengthOfLastWord2(String s) {
+        s = s.trim();
+        int start = s.lastIndexOf(" ") + 1;
+        return s.substring(start).length();
+    }
+
+    public static int lengthOfLastWord3(String s) {
+        s = s.trim();
+        int start = s.lastIndexOf(" ") + 1;
+        int end = s.length()-1;
+        return end - start + 1;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int i  =lengthOfLastWord3("  f   po  ");
+        System.out.println(i);
+    }
+}
---
+++ src/easy/num_66.java
@@ -0,0 +1,43 @@
+package easy;
+
+public class num_66 {
+    public static int[] plusOne(int[] digits) {
+        if (digits.length >0 && digits[0]==0){
+            return new int[]{1};
+        }
+        for (int i = digits.length-1; i>=0; i--) {
+            if (digits[i]!=9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        int[] arr = new int[digits.length + 1];
+        arr[0] = 1;
+        return arr;
+    }
+
+    //String-> int 数会超过范围  思路不可行
+   /* public static int[] plusOne2(int[] digits) {
+        String s="";
+        for (int num : digits
+             ) {
+            s+=num;
+        }
+        int anInt = Integer.parseInt(s);
+        anInt+=1;
+        s = anInt+"";
+        char[] chars = s.toCharArray();
+        int[] intArr = new int[chars.length];
+        for(int i=0;i<chars.length;i++){
+            intArr[i] = Integer.parseInt(chars[i]+"");
+        }
+        return intArr;
+
+    }*/
+
+    public static void main(String[] args) {
+        int[] ints = plusOne(new int[]{9,9,9,9});
+        System.out.println(ints);
+    }
+}
