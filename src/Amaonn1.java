3296de4015188d2856c286afb627d3975dbac2ce
zhulihua <18716000382@163.com>
2024-05-13 09:42:29 +0800

    push

---
+++ src/Amaonn1.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+/**
+ * 代码有问题
+ */
+public class Amaonn1 {
+    public static int removeProducts(int[] prices, int k, int threshold) {
+        Arrays.sort(prices);
+        int start = 0;
+        int end = prices.length - 1;
+        int sum = 0;
+
+        while (start <= end) {
+            sum += prices[start];
+            if (sum <= threshold) {
+                start++;
+            } else {
+                sum -= prices[end];
+                end--;
+            }
+        }
+
+        return Math.max(0, k - (end - start + 1));
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {3, 2, 1, 4, 6, 5};
+        int k = 3;
+        int threshold = 14;
+        int result = removeProducts(prices, k, threshold);
+        System.out.println("需要移除的商品数量为：" + result);
+
+    }
+
+}
---
+++ src/SnapMatrix.java
@@ -0,0 +1,62 @@
+/**
+ * 蛇形数组打印
+ */
+public class SnapMatrix {
+    public static void printSnapMatrix(int n){
+        int[][] matrix = new int[n][n];
+        int num = 1;
+        int row = 0;
+        int col = n-1;
+        int dir = 0; //0->右 1->下 2->左 3->上
+        while(num <= n * n){
+            matrix[row][col] = num++;
+            switch (dir){
+                case 0:
+                    if( col == n-1 || matrix[row][col+1] != 0){
+                        row++;
+                        dir = 1;
+                    } else{
+                        col++;
+                    }
+                    break;
+                case 1:
+                    if(row == n-1 || matrix[row+1][col] != 0){
+                        col--;
+                        dir = 2;
+                    } else{
+                        row++;
+                    }
+                    break;
+                case 2:
+                    if(col ==0 || matrix[row][col-1] != 0){
+                        row--;
+                        dir = 3;
+                    } else{
+                        col--;
+                    }
+                    break;
+                case 3:
+                    if(row == 0 || matrix[row-1][col] != 0){
+                        col++;
+                        dir = 0;
+                    }else{
+                        row--;
+                    }
+                    break;
+            }
+        }
+        for(int i =0; i<n;i++){
+            for (int j = 0; j<n ;j++){
+                System.out.print(matrix[i][j] + " ");
+                if (j == n-1){
+                    System.out.println();
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printSnapMatrix(4);
+    }
+}
